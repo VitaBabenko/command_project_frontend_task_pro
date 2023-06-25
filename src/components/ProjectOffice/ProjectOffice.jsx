9411927156425e6dd73b4665a3ee8cd8dc24f5c7
VitaBabenko <babenkovita1988@gmail.com>
2023-06-25 15:06:19 +0300

    added commit unused values in ProjectOffice

--- src/components/ProjectOffice/ProjectOffice.jsx
+++ src/components/ProjectOffice/ProjectOffice.jsx
@@ -1,41 +1,49 @@
 import FilterAltIcon from '@mui/icons-material/FilterAlt';
-import { FilterWrapper, Title, Wrapper, Text, StackWrapper, ButtonStyled, AddIconStyled } from './ProjectOfficeStyle';
-import { CustomButton } from 'components/Button/CustomButton';
-import { useState } from 'react';
-import { ModalColumn } from 'components/Modal/ModalColumn/ModalColumn';
+import {
+  FilterWrapper,
+  Title,
+  Wrapper,
+  Text,
+  StackWrapper,
+  ButtonStyled,
+  AddIconStyled,
+} from './ProjectOfficeStyle';
+// import { CustomButton } from 'components/Button/CustomButton';
+// import { useState } from 'react';
+// import { ModalColumn } from 'components/Modal/ModalColumn/ModalColumn';
 import ProjectOfficeItem from './ProjectOfficeItem';
 
 const ProjectOffice = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    const columns = [{columnName: 'one', id: 1}, {columnName: 'two', id: 2}];
+  // const [isOpen, setIsOpen] = useState(false);
+  // const columns = [{columnName: 'one', id: 1}, {columnName: 'two', id: 2}];
 
-    const handleToggleModal = () => { 
-        setIsOpen((prevstate) => !prevstate )
-    }
+  // const handleToggleModal = () => {
+  //     setIsOpen((prevstate) => !prevstate )
+  // }
 
-    const handleCreateColumn = (columnName) => { 
-        const newArr = [...columns, { id: 4, columnName: columnName }];
-        console.log(columns)
-    }
+  // const handleCreateColumn = (columnName) => {
+  //     const newArr = [...columns, { id: 4, columnName: columnName }];
+  //     console.log(columns)
+  // }
 
   return (
     <>
-        <Wrapper>
-            <Title>Project office</Title>
-            <FilterWrapper>
-                <FilterAltIcon />
-                <Text>Filters</Text>
-            </FilterWrapper>
-        </Wrapper>
-          <StackWrapper direction="row" spacing={2}>
-              {/* <CustomButton isWhiteBackground={true} onClick={handleToggleModal}>Add</CustomButton>
+      <Wrapper>
+        <Title>Project office</Title>
+        <FilterWrapper>
+          <FilterAltIcon />
+          <Text>Filters</Text>
+        </FilterWrapper>
+      </Wrapper>
+      <StackWrapper direction="row" spacing={2}>
+        {/* <CustomButton isWhiteBackground={true} onClick={handleToggleModal}>Add</CustomButton>
               <ModalColumn isOpen={isOpen} onClose={handleToggleModal} onRemove={handleCreateColumn} /> */}
-            <ButtonStyled startIcon={<AddIconStyled />}>
-                Add another column
-            </ButtonStyled>
-        </StackWrapper>
-        <ProjectOfficeItem />
-    </> 
+        <ButtonStyled startIcon={<AddIconStyled />}>
+          Add another column
+        </ButtonStyled>
+      </StackWrapper>
+      <ProjectOfficeItem />
+    </>
   );
 };
 
