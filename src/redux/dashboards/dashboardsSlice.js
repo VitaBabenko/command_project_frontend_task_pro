185b5522866d9f5618de45cba50956691292e9e0
VitaBabenko <babenkovita1988@gmail.com>
2023-06-26 13:14:48 +0300

    added import fetchUserDashboards

--- src/redux/dashboards/dashboardsSlice.js
+++ src/redux/dashboards/dashboardsSlice.js
@@ -1,26 +1,29 @@
-// import { createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import { fetchUserDashboards } from './operation';
 
-// const dashboardSlice = createSlice({
-//     name: 'dashboard',
-//     initialState: {
-//         dashboards: [],
-//         isLoading: false,
-//         error: null,
-//     },
-//     extraReducers: (builder) => {
-//         builder.addCase(fetchUserDashboards.pending, (state) => {
-//         state.isLoading = true;
-//         state.error = null;
-//       })
-//       .addCase(fetchUserDashboards.fulfilled, (state, action) => {
-//           state.isLoading = false;
-//           console.log(action.payload)
-//         state.dashboards = action.payload;
-//       })
-//       .addCase(fetchUserDashboards.rejected, (state, action) => {
-//         state.isLoading = false;
-//         state.error = action.error.message;
-//       });
-//     }
+const dashboardSlice = createSlice({
+  name: 'dashboard',
+  initialState: {
+    dashboards: [],
+    isLoading: false,
+    error: null,
+  },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchUserDashboards.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchUserDashboards.fulfilled, (state, action) => {
+        state.isLoading = false;
+        console.log(action.payload);
+        state.dashboards = action.payload;
+      })
+      .addCase(fetchUserDashboards.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
+  },
+});
 
-// });
+export default dashboardSlice.reducer;
