af27c4fe5e612e4ca44d5acff34e1bd093d10629
alexveryk <veryk.av@gmail.com>
2023-06-27 01:10:32 +0300

    Create Filter.jsx

---
+++ src/components/PopUpFilter/Filter/Filter.jsx
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import Radio from '@mui/material/Radio';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+
+import {
+  Title,
+  FilterContainer,
+  BtnShowAll,
+  RadioGr,
+  radioButtons,
+} from './Filter.styled';
+
+export const Filter = () => {
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const primaryTextColor = `var(--primaryTextColor)`;
+
+  const handleChange = event => {
+    setSelectedValue(event.target.value);
+  };
+
+  const handleReset = () => {
+    setSelectedValue('');
+  };
+
+  return (
+    <>
+      <FilterContainer>
+        <Title>Label color</Title>
+        <BtnShowAll type="button" onClick={handleReset}>
+          Show all
+        </BtnShowAll>
+      </FilterContainer>
+      <FormControl>
+        <RadioGr
+          aria-labelledby="priority"
+          name="radio-buttons-group"
+          value={selectedValue}
+          onChange={handleChange}
+        >
+          {radioButtons.map(button => (
+            <FormControlLabel
+              key={button.value}
+              sx={{
+                color: button.color,
+                '& .Mui-checked.MuiRadio-root': {
+                  color: button.checkedColor,
+                },
+                '& .MuiSvgIcon-root': {
+                  fontSize: 14,
+                },
+                '& .MuiFormControlLabel-label': {
+                  fontSize: 12,
+                },
+                '& .MuiButtonBase-root.MuiRadio-root': {
+                  color: button.checkedColor,
+                  padding: 0,
+                  backgroundColor:
+                    selectedValue === button.value
+                      ? 'transparent'
+                      : button.buttonColor,
+                },
+                '& .MuiTypography-root': {
+                  marginLeft: 1,
+                  color:
+                    selectedValue === button.value
+                      ? primaryTextColor
+                      : 'inherit',
+                },
+              }}
+              value={button.value}
+              control={<Radio />}
+              label={button.label}
+            />
+          ))}
+        </RadioGr>
+      </FormControl>
+    </>
+  );
+};
