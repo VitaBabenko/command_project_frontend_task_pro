0e142bff888aea42c9eee6e0e4d119de91fbf6ff
Viacheslav Borysov <s.borisovbar@gmail.com>
2023-06-27 02:19:40 +0300

    add in dashboardSlice action with dashboard

--- src/components/ProjectOffice/ProjectOffice.jsx
+++ src/components/ProjectOffice/ProjectOffice.jsx
@@ -1,28 +1,28 @@
 import { useState } from 'react';
 
 import ProjectOfficeItem from './ProjectOfficeItem';
-import { useEffect } from 'react';
-import { getColumns } from 'services/fetchColumn';
+// import { useEffect } from 'react';
+// import { getColumns } from 'services/fetchColumn';
 import ProjectOfficeHeader from './ProjectOfficeHeader/ProjectOfficeHeader';
 import AddColumnButton from './AddColumnsButton/AddColumnsButton';
 
 const ProjectOffice = () => {
   const [columns, setColumns] = useState([]);
-  const id = '9a5c9bf5-64d7-42dc-9279-d22fde663db5';
+  // const id = '9a5c9bf5-64d7-42dc-9279-d22fde663db5';
 
-  useEffect(() => {
-    const getBoardColumn = async () => {
-      try {
-        const boardColumns = await getColumns(id);
-        setColumns(boardColumns);
-      } catch (error) {
-        console.log(error.message);
-      }
-    };
-    getBoardColumn();
-  }, [id]);
+  // useEffect(() => {
+  //   const getBoardColumn = async () => {
+  //     try {
+  //       const boardColumns = await getColumns(id);
+  //       setColumns(boardColumns);
+  //     } catch (error) {
+  //       console.log(error.message);
+  //     }
+  //   };
+  //   getBoardColumn();
+  // }, [id]);
 
-  console.log(columns);
+  // console.log(columns);
 
   return (
     <>
