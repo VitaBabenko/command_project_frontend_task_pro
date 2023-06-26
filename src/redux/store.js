d527e3a501000ef7df6bc52f8068177128d908d9
Oleksandr Druzhynin <aleks_str78@ukr.net>
2023-06-27 00:13:00 +0300

    write theme to back end state

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import themeReducer from './theme/themeSlice';
 import modalNameReducer from './modal';
 import {authReducer} from './Auth/slice'
 
 const store = configureStore({
   reducer: {
-    theme: themeReducer,
-    modal: modalNameReducer
+    modal: modalNameReducer,
     auth: authReducer,
   },
 });
