4501f59514e8705bebf645f425ec96f93f6d593f
Viacheslav Borysov <s.borisovbar@gmail.com>
2023-06-26 12:15:36 +0300

    add fetch

--- src/redux/dashboards/dashboardsSlice.js
+++ src/redux/dashboards/dashboardsSlice.js
@@ -8,7 +8,19 @@ const dashboardSlice = createSlice({
         error: null,
     },
     extraReducers: (builder) => {
-        builder
+        builder.addCase(fetchUserDashboards.pending, (state) => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(fetchUserDashboards.fulfilled, (state, action) => {
+          state.isLoading = false;
+          console.log(action.payload)
+        state.dashboards = action.payload;
+      })
+      .addCase(fetchUserDashboards.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
     }
 
 });
--- src/redux/dashboards/operation.js
+++ src/redux/dashboards/operation.js
@@ -5,9 +5,12 @@ axios.defaults.baseURL = 'https://64975d0e83d4c69925a39dad.mockapi.io';
 
 export const fetchUserDashboards = createAsyncThunk('dashboards/fethUserDashboards', async (userId, { rejectWithValue }) => { 
     try {
-        const resp = axios.get(`/dashboards/${userId}`)
-        console.log(resp)
-        return resp
+        const resp = await axios.get('/dashboards', {
+            params: {
+                owner: userId
+            }
+        })
+        return resp.data
     } catch (error) {
         return console.log(error)
     }
