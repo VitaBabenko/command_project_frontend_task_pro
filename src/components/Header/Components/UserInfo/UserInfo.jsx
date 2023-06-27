0d96ff74cc084d90fc8ebca65e92ee6653b5aa90
VitaBabenko <babenkovita1988@gmail.com>
2023-06-27 13:35:27 +0300

    added alt to img

--- src/components/Header/Components/UserInfo/UserInfo.jsx
+++ src/components/Header/Components/UserInfo/UserInfo.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 
-export const UserInfo = ({
-  name,
-  avatarUrl
-}) => {
+export const UserInfo = ({ name, avatarUrl }) => {
   return (
     <div className="header-user-info">
       <p>{name}</p>
-      {avatarUrl && <div className="img-wrap"><img src={avatarUrl} /></div>}
+      {avatarUrl && (
+        <div className="img-wrap">
+          <img src={avatarUrl} alt="user avatar" />
+        </div>
+      )}
     </div>
   );
 };
