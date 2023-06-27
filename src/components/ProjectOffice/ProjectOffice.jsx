933183624deaee758ee11560cf337450ae4e8861
VitaBabenko <babenkovita1988@gmail.com>
2023-06-27 09:46:35 +0300

    comment projectOffice

--- src/components/ProjectOffice/ProjectOffice.jsx
+++ src/components/ProjectOffice/ProjectOffice.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
-import ProjectOfficeItem from './ProjectOfficeItem';
+// import ProjectOfficeItem from './ProjectOfficeItem';
 // import { useEffect } from 'react';
 // import { getColumns } from 'services/fetchColumn';
 import ProjectOfficeHeader from './ProjectOfficeHeader/ProjectOfficeHeader';
 import AddColumnButton from './AddColumnsButton/AddColumnsButton';
 
 const ProjectOffice = () => {
-  const [columns, setColumns] = useState([]);
+  // const [columns, setColumns] = useState([]);
   // const id = '9a5c9bf5-64d7-42dc-9279-d22fde663db5';
 
   // useEffect(() => {
@@ -28,9 +28,13 @@ const ProjectOffice = () => {
     <>
       <ProjectOfficeHeader />
       <AddColumnButton />
-      {columns.map(column => {
-        return <ProjectOfficeItem column={column} key={column.id} />;
-      })}
+      {/* {columns.map(column => {
+        return (
+          <ProjectOfficeItem
+          column={column} key={column.id}
+          />
+        );
+      })} */}
       {/* <ProjectOfficeItem column={columns} /> */}
     </>
   );
