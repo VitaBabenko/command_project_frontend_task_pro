9bf47786ee80ca9eaf42dec38a04e7dcfe143fc4
Vita Babenko <110460253+VitaBabenko@users.noreply.github.com>
2023-06-28 13:16:29 +0300

    Update Board.styled.jsx

--- src/components/Sidebar/components/Boards/Board.styled.jsx
+++ src/components/Sidebar/components/Boards/Board.styled.jsx
@@ -1,68 +1,68 @@
 import styled from 'styled-components';
 
 export const LogoLink = styled.div`
-  color: inherit
+  color: inherit;
 `;
 
 export const SidebarBoards = styled.div`
-  margin-top: 64px
+  margin-top: 64px;
 `;
 
 export const TitleMin = styled.span`
-  margin-bottom: 8px
-      color: rgba(255, 255, 255, 0.50)
-      font-size: 12px
-      display: block
+  margin-bottom: 8px;
+      color: rgba(255, 255, 255, 0.50);
+      font-size: 12px;
+      display: block;
 `;
 
 export const SidebarCreateNew = styled.div`
- color: #FFFFFF
-    margin-right: 24px
-    display: flex
-    align-items: center
-    justify-content: space-between
-    padding: 14px 0
-    font-size: 14px
-    border-top: 1px solid rgba(255,255,255,0.1)
-    border-bottom: 1px solid rgba(255,255,255,0.1)
+ color: #FFFFFF;
+    margin-right: 24px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 14px 0;
+    font-size: 14px;
+    border-top: 1px solid rgba(255,255,255,0.1);
+    border-bottom: 1px solid rgba(255,255,255,0.1);
 `;
 
 export const NewBoardBtn = styled.div`
- width: 40px
-      height: 36px
-      display: flex
-      align-items: center
-      justify-content: center
-      font-size: 20px
-      border-radius: 6px
-      background: #BEDBB0
-      line-height: 1
-      color: #121212
+ width: 40px;
+      height: 36px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-size: 20px;
+      border-radius: 6px;
+      background: #BEDBB0;
+      line-height: 1;
+      color: #121212;
 `;
 
 export const SidebarPrjctList = styled.ul`
-  margin-top: 40px
-    margin-bottom: 40px
+  margin-top: 40px;
+    margin-bottom: 40px;
 `;
 
 export const PrjctItemMain = styled.div`
-  padding: 20px 20px 20px 0
-    display: flex
-    align-items: center
-    justify-content: space-between
-    font-size: 14px
-    color: rgba(255,255,255,0.5)
-    position: relative
-    overflow: hidden
+  padding: 20px 20px 20px 0;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 14px;
+    color: rgba(255,255,255,0.5);
+    position: relative;
+    overflow: hidden;
 `;
 
 export const FlexElems = styled.div`
- display: flex
-      align-items: center
+ display: flex;
+      align-items: center;
 `;
 
 export const IconWrap = styled.div`
- margin-right: 8px
-      display: inline-block
+ margin-right: 8px;
+      display: inline-block;
 `;
 
