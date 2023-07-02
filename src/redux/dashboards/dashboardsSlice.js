889d9aef0e48d1ab5a42001bcebecf866a8cc57b
VitaBabenko <babenkovita1988@gmail.com>
2023-07-02 16:59:03 +0300

    conflict done

--- src/redux/dashboards/dashboardsSlice.js
+++ src/redux/dashboards/dashboardsSlice.js
@@ -6,6 +6,8 @@ import {
   fetchUserDashboards,
   getColumnsForBoard,
   updateUserBoard,
+  updateColumn,
+  deleteColumn,
 } from './operation';
 
 const dashboardSlice = createSlice({
@@ -122,37 +124,52 @@ const dashboardSlice = createSlice({
       .addCase(addColumn.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.error.message;
-      }).addCase(updateColumn.pending, state => {
+      })
+      .addCase(updateColumn.pending, state => {
         state.isLoading = true;
         state.error = null;
-      }).addCase(updateColumn.fulfilled, (state, action) => {
+      })
+      .addCase(updateColumn.fulfilled, (state, action) => {
         state.isLoading = false;
         const editColumn = action.payload;
-        const index = state.dashboards.findIndex(board => board._id === editColumn.boardId);
+        const index = state.dashboards.findIndex(
+          board => board._id === editColumn.boardId
+        );
         if (index !== -1) {
-          const columnIndex = state.dashboards[index].columns.findIndex(column => column._id === editColumn._id);
+          const columnIndex = state.dashboards[index].columns.findIndex(
+            column => column._id === editColumn._id
+          );
           if (columnIndex !== -1) {
-            state.dashboards[index].columns[columnIndex].title = editColumn.title;
+            state.dashboards[index].columns[columnIndex].title =
+              editColumn.title;
           }
         }
-      }).addCase(updateColumn.rejected, (state, action) => {
+      })
+      .addCase(updateColumn.rejected, (state, action) => {
         state.isLoading = false;
         state.error = action.payload.message;
-      }).addCase(deleteColumn.pending, state => {
+      })
+      .addCase(deleteColumn.pending, state => {
         state.isLoading = true;
         state.error = null;
-      }).addCase(deleteColumn.fulfilled, (state, action) => {
+      })
+      .addCase(deleteColumn.fulfilled, (state, action) => {
         state.isLoading = false;
         state.error = null;
         const { columnId, boardId } = action.payload;
-        const boardIndex = state.dashboards.findIndex(board => board._id === boardId);
+        const boardIndex = state.dashboards.findIndex(
+          board => board._id === boardId
+        );
         if (boardIndex !== -1) {
-          const columnIndex = state.dashboards[boardIndex].columns.findIndex(column => column._id === columnId);
+          const columnIndex = state.dashboards[boardIndex].columns.findIndex(
+            column => column._id === columnId
+          );
           if (columnIndex !== -1) {
             state.dashboards[boardIndex].columns.splice(columnIndex, 1);
           }
         }
-      }).addCase(deleteColumn.rejected, (state, action) => { 
+      })
+      .addCase(deleteColumn.rejected, (state, action) => {
         state.isLoading = false;
         state.error = state.payload.message;
       });
