201faaef547067a3d64af27a8802efb5f09dca22
VitaBabenko <babenkovita1988@gmail.com>
2023-07-02 13:20:13 +0300

    conflict done

--- src/components/ProjectOffice/ProjectOfficeItem/ProjectOfficeItem.jsx
+++ src/components/ProjectOffice/ProjectOfficeItem/ProjectOfficeItem.jsx
@@ -3,54 +3,52 @@ import ProjectOfficeCardItem from '../ProjectOfficeCardItem/ProjectOfficeCardIte
 import { ActionsButton, StyledTitle } from './OfficeItemStyle.styled';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { ModalColumn } from 'components/Modal/ModalColumn/ModalColumn';
 import { useDispatch } from 'react-redux';
 import { deleteColumn, updateColumn } from 'redux/dashboards/operation';
-
 
 const ProjectOfficeItem = ({ column: { title, _id }, boardId }) => {
   const [isOpen, setIsOpen] = useState(false);
 
   const dispatch = useDispatch();
 
-  const handleEditColumnName = (newName) => { 
-
+  const handleEditColumnName = newName => {
     dispatch(updateColumn({ boardId, columnId: _id, title: newName }));
-  } 
+  };
 
-  const handleDeletecolumn = () => { 
+  const handleDeletecolumn = () => {
     dispatch(deleteColumn({ boardId, columnId: _id }));
+  };
 
-  }
-
-
-
-
-  const handleToggle = () => { 
+  const handleToggle = () => {
     setIsOpen(!isOpen);
-  } 
+  };
   // console.log(_id)
-  console.log(title)
+  console.log(title);
   return (
     <>
       <Wrapper>
-            <ActionsButton>
-            <StyledTitle>{ title }</StyledTitle>
+        <ActionsButton>
+          <StyledTitle>{title}</StyledTitle>
           <ActionsIconsButton>
-            <button type='button' onClick={handleToggle}>
-              <EditIcon fontSize='small' sx={{ color: '#888888' }} />
+            <button type="button" onClick={handleToggle}>
+              <EditIcon fontSize="small" sx={{ color: '#888888' }} />
             </button>
-            <button type='button' onClick={handleDeletecolumn}>
-              <DeleteOutlineIcon fontSize='small' sx={{ color: '#888888' }} />
-              </button>
-            </ActionsIconsButton>
+            <button type="button" onClick={handleDeletecolumn}>
+              <DeleteOutlineIcon fontSize="small" sx={{ color: '#888888' }} />
+            </button>
+          </ActionsIconsButton>
         </ActionsButton>
       </Wrapper>
-      <ModalColumn isOpen={isOpen} onClose={handleToggle} type='edit' onUpdate={handleEditColumnName} />
-        <ProjectOfficeCardItem />
-
-    </> 
+      <ModalColumn
+        isOpen={isOpen}
+        onClose={handleToggle}
+        type="edit"
+        onUpdate={handleEditColumnName}
+      />
+      <ProjectOfficeCardItem />
+    </>
   );
 };
 
