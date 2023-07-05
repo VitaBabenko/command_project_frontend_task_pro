af3d715b51f8e98a5297c6268c515d8eba9012f5
VitaBabenko <babenkovita1988@gmail.com>
2023-07-05 11:18:54 +0300

    fix background color in default screen

--- src/components/DefaultScreen/DefaultScreen.jsx
+++ src/components/DefaultScreen/DefaultScreen.jsx
@@ -1,11 +1,22 @@
-import { AccentText, DefaultText, DefaultWrapper } from "./DefaultScreen.styled"
+import {
+  AccentText,
+  DefaultText,
+  DefaultWrapper,
+  Wrap,
+} from './DefaultScreen.styled';
 
 export const DefaultScreen = () => {
-
-    return (
-        
-        <DefaultWrapper>
-            <DefaultText>Before starting your project, it is essential <AccentText>to create a board</AccentText> to visualize and track all the necessary tasks and milestones. This board serves as a powerful tool to organize the workflow and ensure effective collaboration among team members.</DefaultText>
-        </DefaultWrapper>
-    )
-}
+  return (
+    <Wrap>
+      <DefaultWrapper>
+        <DefaultText>
+          Before starting your project, it is essential{' '}
+          <AccentText>to create a board</AccentText> to visualize and track all
+          the necessary tasks and milestones. This board serves as a powerful
+          tool to organize the workflow and ensure effective collaboration among
+          team members.
+        </DefaultText>
+      </DefaultWrapper>
+    </Wrap>
+  );
+};
